e95a538f5bc7c85af21e36635c1abbb8cd65f0ca
Pia <76558220+rkdud007@users.noreply.github.com>
2023-08-19 19:58:25 +0800

    basic command🦀

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,116 @@
 use anyhow::Result;
+use chrono::prelude::*;
+use starknet::core::types::MaybePendingBlockWithTxHashes::{Block, PendingBlock};
+use starknet::providers::ProviderError::{ArrayLengthMismatch, Other, RateLimited, StarknetError};
+use starknet::{
+    core::types::BlockId,
+    providers::{Provider, SequencerGatewayProvider},
+};
 
-pub async fn blocknumber_to_timestamp(blocknumber: u64) -> Result<String> {
-    Ok("aagweg".to_string())
+pub async fn blocknumber_to_timestamp(
+    provider: &SequencerGatewayProvider,
+    blocknumber: u64,
+) -> Result<u64> {
+    let current_timstamp = get_block_timestamp(&provider, blocknumber).await?;
+    Ok(current_timstamp)
 }
 
-pub async fn timestamp_to_blocknumber(timestamp: String) -> Result<u64> {
-    Ok(100)
+pub async fn timestamp_to_blocknumber(
+    provider: &SequencerGatewayProvider,
+    timestamp: u64,
+) -> Result<u64> {
+    let blocknumber = block_search(&provider, timestamp).await?;
+    Ok(blocknumber)
+}
+
+// refered from https://github.com/0xcacti/snipe
+async fn block_search(provider: &SequencerGatewayProvider, target_timestamp: u64) -> Result<u64> {
+    let current_blocknumber = get_current_block_number(&provider).await?;
+    let current_timstamp = get_block_timestamp(&provider, current_blocknumber).await?;
+
+    let genesis_timestamp = get_block_timestamp(&provider, 0).await?;
+    if target_timestamp < genesis_timestamp {
+        return Err(anyhow::anyhow!(
+            "Time is before genesis block's time: {}/{}",
+            timestamp_to_unix(genesis_timestamp),
+            genesis_timestamp
+        ));
+    }
+    if target_timestamp > current_timstamp {
+        return Err(anyhow::anyhow!(
+            "Time is in the future, Latest time is:{}/{}",
+            timestamp_to_unix(current_timstamp),
+            current_timstamp
+        ));
+    }
+
+    let mut lower_bound = 0;
+    let mut upper_bound = current_blocknumber;
+    let mut current_blocknumber = current_blocknumber / 2;
+    let mut current_timstamp = get_block_timestamp(&provider, current_blocknumber).await?;
+
+    while lower_bound <= upper_bound {
+        if current_timstamp == target_timestamp {
+            return Ok(current_blocknumber);
+        }
+
+        if current_timstamp < target_timestamp {
+            lower_bound = current_blocknumber + 1;
+        } else {
+            upper_bound = current_blocknumber - 1;
+        }
+        current_blocknumber = (lower_bound + upper_bound) / 2;
+        current_timstamp = get_block_timestamp(&provider, current_blocknumber).await?;
+    }
+    if lower_bound > upper_bound {
+        current_blocknumber = lower_bound;
+    }
+    Ok(current_blocknumber)
+}
+
+async fn get_block_timestamp(
+    provider: &SequencerGatewayProvider,
+    block_number: u64,
+) -> Result<u64> {
+    let block = provider
+        .get_block_with_tx_hashes(BlockId::Number(block_number))
+        .await;
+
+    match block {
+        Ok(full_block) => match full_block {
+            Block(finalized_block) => {
+                let timestamp = finalized_block.timestamp;
+                Ok(timestamp)
+            }
+            PendingBlock(_) => Err(anyhow::anyhow!("No pending block in starknet")),
+        },
+        Err(err) => match err {
+            StarknetError(e) => {
+                println!("{:#?}", e.message);
+                Err(anyhow::anyhow!(e.message))
+            }
+            RateLimited => {
+                println!("{:#?}", "Rate Limited");
+                Err(anyhow::anyhow!("RateLimited"))
+            }
+            ArrayLengthMismatch => {
+                println!("{:#?}", "Array Length mismatch");
+                Err(anyhow::anyhow!("ArrayLengthMismatch"))
+            }
+            Other(_) => {
+                println!("{:#?}", "othererror");
+                Err(anyhow::anyhow!("Other Error"))
+            }
+        },
+    }
+}
+
+async fn get_current_block_number(provider: &SequencerGatewayProvider) -> Result<u64> {
+    let block_number = provider.block_number().await?;
+    Ok(block_number)
+}
+
+pub fn timestamp_to_unix(timestamp: u64) -> DateTime<Utc> {
+    let date_time = Utc.timestamp_opt(timestamp as i64, 0).unwrap();
+    date_time
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
-use woodcock::{blocknumber_to_timestamp, timestamp_to_blocknumber};
+use starknet::providers::SequencerGatewayProvider;
+
+use woodcock::{blocknumber_to_timestamp, timestamp_to_blocknumber, timestamp_to_unix};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -7,16 +9,19 @@ struct Cli {
     #[command(subcommand)]
     command: Option<Commands>,
 
+    // #[arg(short, long)]
+    // #[arg(value_name = "TIME_ZONE")]
+    // #[arg(help = "Timezone")]
+    // timezone: Option<String>,
     #[arg(short, long)]
-    #[arg(value_name = "TIME_ZONE")]
-    #[arg(help = "Timezone")]
-    timezone: Option<String>,
+    #[arg(value_name = "NETWORK")]
+    #[arg(help = "Network: [mainnet/goerli/goerli2]")]
+    network: Option<String>,
 
-    #[arg(short = 'u', long)]
-    #[arg(env = "STARKNET_RPC_URL")]
-    #[arg(help = "The RPC endpoint")]
-    rpc_url: String,
-
+    // #[arg(short = 'u', long)]
+    // #[arg(value_name = "RPC_URL")]
+    // #[arg(help = "The RPC endpoint")]
+    // rpc_url: Option<String>,
     #[arg(short, long)]
     #[arg(value_name = "TIME_FORMAT")]
     #[arg(help = "The format to use time")]
@@ -42,23 +47,72 @@ enum Commands {
     TimeToBlock {
         /// The time to convert
         #[arg(required = true)]
-        time: String,
+        time: u64,
     },
 }
 
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
-    println!("{:#?}", args);
+
+    let mut provider = SequencerGatewayProvider::starknet_alpha_mainnet();
+    match args.network {
+        Some(network) => {
+            if network == "goerli" {
+                println!("{}", "Use goerli network as provider");
+                provider = SequencerGatewayProvider::starknet_alpha_goerli();
+            } else if network == "goerli2" {
+                println!("{}", "Use goerli2 network as provider");
+                provider = SequencerGatewayProvider::starknet_alpha_goerli_2();
+            }
+        }
+        None => {
+            println!("{}", "Default network is mainnet");
+        }
+    }
+
+    // match args.rpc_url {
+    //     Some(url) => {
+    //         let rpc_client = JsonRpcClient::new(HttpTransport::new(Url::parse(&url).unwrap()));
+    //         let time_result = blocknumber_to_timestamp(&rpc_client, block_number).await;
+    //     }
+    //     None => {}
+    // }
 
     match args.command {
         Some(Commands::BlockToTime { block_number }) => {
-            let time_result = blocknumber_to_timestamp(block_number).await;
-            println!("{}", time_result.unwrap());
+            let time_result = blocknumber_to_timestamp(&provider, block_number).await;
+            match time_result {
+                Ok(time_result) => match args.format {
+                    Some(format) => {
+                        if format == "unix" {
+                            println!("{}", timestamp_to_unix(time_result));
+                        } else {
+                            println!("{}", "Unvalid Format");
+                            println!("{}", time_result);
+                        }
+                    }
+                    None => {
+                        println!(
+                            "Blocknumber #{} is created at {}.",
+                            block_number, time_result
+                        );
+                    }
+                },
+                Err(err) => println!("{}", err),
+            }
         }
         Some(Commands::TimeToBlock { time }) => {
-            let block_result = timestamp_to_blocknumber(time).await;
-            println!("{}", block_result.unwrap());
+            let block_result = timestamp_to_blocknumber(&provider, time).await;
+            match block_result {
+                Ok(block_result) => {
+                    println!(
+                        "Most closest block that created at time {} is: #{}",
+                        time, block_result
+                    );
+                }
+                Err(err) => println!("{}", err),
+            }
         }
         None => {}
     }
